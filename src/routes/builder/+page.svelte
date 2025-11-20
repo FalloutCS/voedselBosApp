e83f4512bec2629f0458f8a68630d46fbedc4eae
Ruben Verhoef <ruben.leopold@gmail.com>
2025-11-20 10:14:13 +0100

    added basic app.css, added types.ts, created /builder route

--- src/routes/builder/+page.svelte
+++ src/routes/builder/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-    import type { PageProps } from './$types';
-
-    let { data }: PageProps = $props();
+  import type { PageProps } from "./$types";
+  let { data }: PageProps = $props();
 </script>
+
+<div class="w-4/5 h-4/5 bg-violet-100 grid grid-cols-10 grid-rows-10 mx-auto my-auto p-10">
+  <div class=" bg-violet-200 border border-violet-950"></div>
+</div>
