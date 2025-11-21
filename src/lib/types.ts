25c19b9b9adaec4e1057303825922f8d2a90d5a9
FalloutCS <ruben.leopold@gmail.com>
2025-11-21 11:44:15 +0100

    Improved types.ts file, removed server side logic from front-end.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,16 +1,29 @@
 export class Voedselbos {
   name: string;
+  width: number;
+  height: number;
   canvas: voedselbos_Cel[];
 
-  constructor(name: string) {
+  constructor(name: string, width: number, height: number) {
     this.name = name;
+    this.width = width
+    this.height = height
     this.canvas = [];
   }
 
-  populateForest(size: number) {
-    for (let index = 1; index < size; index++) {
+  populateForest() {
+    const totalSize = this.width * this.height
+
+    for (let index = 1; index < totalSize; index++) {
       this.canvas.push({ isPopulated: false, plant: undefined });
     }
+  }
+
+  getCellIndex(pos_X: number, pos_Y: number){
+    if (pos_X < 0 || pos_X >= this.width || pos_Y < 0 || pos_Y >= this.height) {
+      throw new Error("Coordinates are out of bounds")
+    }
+    return (pos_Y * this.width) - (this.width - pos_X)
   }
 }
 
--- src/routes/builder/+page.svelte
+++ src/routes/builder/+page.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
   import type { PageProps } from "./$types";
-  import { Voedselbos, type Plant } from "$lib/types";
   let { data }: PageProps = $props();
-
-  let mijnVoedselbos = new Voedselbos("mijn_Voedselbos");
-
-  mijnVoedselbos.populateForest(100);
-
-  let aardbei: Plant = {
-    name: "Aarbei",
-    img: "/img",
-  };
-
-  mijnVoedselbos.canvas[5].isPopulated = true
-  mijnVoedselbos.canvas[5].plant = aardbei
 </script>
 
+
+<!-- 
 <div
   class="w-4/5 h-4/5 bg-violet-100 grid grid-cols-10 grid-rows-10 mx-auto my-auto p-10"
 >
@@ -25,4 +14,4 @@
     {/if}
     <div class=" bg-violet-200 border border-violet-950"></div>
   {/each}
-</div>
+</div> -->
