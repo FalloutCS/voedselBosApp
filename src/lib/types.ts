d60297b54b1a719591f89bc034ebd346a3bea03a
Siu1 <tinepangsiu@gmail.com>
2025-12-01 13:11:22 +0100

    added Grass type.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -31,7 +31,7 @@ export type Plant = {
   id: number;
   latinName: string;
   commonName: string;
-  habit: "Annual" | "Annual/Biennial" | "Annual Climber" | "Annual/Perennial" | "Bamboo" | "Biennial" | "Biennial/Perennial" | "Bulb" | "Climber" | "Corm" | "Fern" | "Lichen" | "Nothofagus Obliqua" | "Perennial" | "Perennial Climber" | "Shrub" | "Tree" | undefined;
+  habit: "Annual" | "Annual/Biennial" | "Annual Climber" | "Annual/Perennial" | "Bamboo" | "Biennial" | "Biennial/Perennial" | "Bulb" | "Climber" | "Corm" | "Fern" | "Grass" | "Lichen" | "Nothofagus Obliqua" | "Perennial" | "Perennial Climber" | "Shrub" | "Tree" | undefined;
   wind: "N" | "W" | "M" | "F" | undefined;
 };
 
