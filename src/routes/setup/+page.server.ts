757f64a7fa0af60623314a85d7474d64d2353b31
Ruben Verhoef <ruben.leopold@gmail.com>
2025-12-12 16:02:30 +0100

    /setup route gemaakt en heel simpele set-up zonder styling

---
+++ src/routes/setup/+page.server.ts
@@ -0,0 +1,18 @@
+import { forestStore } from "$lib/server/db/forestStore.js";
+import type { PageServerLoad } from "./$types";
+
+export const load = (async () => {
+  return {};
+}) satisfies PageServerLoad;
+
+export const actions = {
+  create: async ({ request }) => {
+    const data = await request.formData();
+    const name = String(data.get("name"));
+    const location = String(data.get("location"));
+    const width = Number(data.get("width"));
+    const heigth = Number(data.get("heigth"));
+
+    forestStore.create(name, width, heigth, location);
+  },
+};
---
+++ src/routes/setup/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { PageProps } from "./$types";
+
+  let { data }: PageProps = $props();
+</script>
+
+<form method="POST">
+  <label for="name">Forest name:</label>
+  <input id="name" type="text" name="name" class="border" required />
+  <label for="location">Forest location:</label>
+  <input id="location" type="text" name="location" class="border" required />
+  <label for="width">Total Width (m)</label>
+  <input id="width" name="width" type="number" class="border" />
+  <label for="heigth">Total heigth (m)</label>
+  <input id="heigth" name="heigth" type="number" class="border" />
+  <button type="submit" formaction="?/create">Maak</button>
+</form>
