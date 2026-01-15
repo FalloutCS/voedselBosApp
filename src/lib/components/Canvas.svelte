7587e9a9808e24cc3354fdaeece33e799940a309
FalloutCS <ruben.leopold@gmail.com>
2026-01-15 12:14:04 +0100

    added a right click menu on canvas to select different shovel options

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -2,6 +2,13 @@
   import { enhance } from "$app/forms";
   import { gethabitIcon } from "$lib/habitIcon";
   import type { PlacedPlant } from "$lib/types";
+
+  import { ContextMenu } from "bits-ui";
+  import CopySimple from "phosphor-svelte/lib/CopySimple";
+  import MouseSimple from "phosphor-svelte/lib/MouseSimple";
+  import PencilSimpleLine from "phosphor-svelte/lib/PencilSimpleLine";
+  import PlusCircle from "phosphor-svelte/lib/PlusCircle";
+  import Trash from "phosphor-svelte/lib/Trash";
   type canvasProps = {
     placedPlants: PlacedPlant;
     surfaceArea: number;
@@ -24,6 +31,9 @@
     stressMap = {},
   }: canvasProps = $props();
 
+  const values = ["one", "two", "three"];
+  let value = $state("one");
+
   function getStressStyle(index: number): string {
     const count = stressMap[index] || 0;
 
@@ -35,35 +45,62 @@
   }
 </script>
 
-<form
-  class="p-5 grid rounded mx-auto my-auto h-full w-full bg-emerald-100"
-  style="grid-template-columns: repeat({width}, minmax(0, 1fr)); grid-template-rows: repeat({height}, minmax(0, 1fr));"
-  method="POST"
-  use:enhance
->
-  {#each { length: surfaceArea }, index}
-    {@const isShovel = editMode === "shovel" ? true : false}
-    {@const isBlocked = shapeArray.includes(index)}
-    {@const stressStyle = getStressStyle(index)}
-
-    <button
-      formaction="?/disableCell"
-      name="cellIndex"
-      value={index}
-      type={isShovel ? "submit" : "button"}
-      onclick={() => (isShovel ? undefined : openMenu(index))}
-      class="border border-green-900 text-sm relative transition-all duration-300"
-      style="{isBlocked ? 'background-color: #0d542b;' : ''} {stressStyle}"
-      disabled={isBlocked && !isShovel}
+<ContextMenu.Root>
+  <ContextMenu.Trigger class="mx-auto my-auto h-full w-full">
+    <form
+      class="p-5 grid rounded mx-auto my-auto h-full w-full bg-emerald-100"
+      style="grid-template-columns: repeat({width}, minmax(0, 1fr)); grid-template-rows: repeat({height}, minmax(0, 1fr));"
+      method="POST"
+      use:enhance
     >
-      {#if placedPlants[index] && placedPlants[index].plant}
-        <img
-          src={gethabitIcon(placedPlants[index].plant.habit)}
-          alt={placedPlants[index].plant.habit}
-          title={placedPlants[index].plant.commonName}
-          class="w-full h-full object-contain pointer-events-none"
-        />
+      {#each { length: surfaceArea }, index}
+        {@const isShovel = editMode === "shovel" ? true : false}
+        {@const isBlocked = shapeArray.includes(index)}
+        {@const stressStyle = getStressStyle(index)}
+
+        <button
+          formaction="?/disableCell"
+          name="cellIndex"
+          value={index}
+          type={isShovel ? "submit" : "button"}
+          onclick={() => (isShovel ? undefined : openMenu(index))}
+          class="border border-gray-800/25 text-sm relative transition-all duration-300"
+          style="{isBlocked ? 'background-color: #1e2939;' : ''} {stressStyle}"
+          disabled={isBlocked && !isShovel}
+        >
+          {#if placedPlants[index] && placedPlants[index].plant}
+            <img
+              src={gethabitIcon(placedPlants[index].plant.habit)}
+              alt={placedPlants[index].plant.habit}
+              title={placedPlants[index].plant.commonName}
+              class="w-full h-full object-contain pointer-events-none"
+            />
+          {/if}
+        </button>
+      {/each}
+    </form>
+  </ContextMenu.Trigger>
+  <ContextMenu.Portal>
+    <ContextMenu.Content
+      class=" bg-green-200 shadow-2xl w-[229px] rounded-xl border px-1 py-1.5 outline-none focus-visible:outline-none"
+    >
+      {#if editMode === "shovel"}
+        <ContextMenu.RadioGroup bind:value>
+          {#each values as value}
+            <ContextMenu.RadioItem {value}>
+              {#snippet children({ checked })}
+                {#if checked}
+                  ✅
+                {/if}
+                {value}
+              {/snippet}
+            </ContextMenu.RadioItem>
+          {/each}
+        </ContextMenu.RadioGroup>
+
+        <ContextMenu.Separator />
+        <ContextMenu.Arrow />
       {/if}
-    </button>
-  {/each}
-</form>
+    </ContextMenu.Content>
+  </ContextMenu.Portal>
+</ContextMenu.Root>
