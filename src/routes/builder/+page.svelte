11467dc26b64ea820faae940dfa80985006818f3
Ruben Verhoef <ruben.leopold@gmail.com>
2026-01-16 13:55:25 +0100

    changed default tool to be viewer. Reordered the tool icons

--- src/routes/builder/+page.svelte
+++ src/routes/builder/+page.svelte
@@ -15,7 +15,7 @@
   let menuState: menuMode = $state("");
   let activeCellIndex: number = $state(0);
   let loading = $state(false);
-  let editMode: "shovel" | "planter" | "view" = $state("shovel");
+  let editMode: "shovel" | "planter" | "view" = $state("view");
   let selectedPlantName = $derived(
     data.placedPlants[activeCellIndex]?.plant?.commonName || "Plant"
   );
--- src/routes/builder/components/Toolbar.svelte
+++ src/routes/builder/components/Toolbar.svelte
@@ -14,6 +14,13 @@
     class="flex items-center gap-x-0.5"
   >
     <Toolbar.GroupItem
+      aria-label="toggle viewer"
+      value="view"
+      class="rounded-2xl hover:bg-violet-400 active:bg-violet-300 data-[state=on]:bg-violet-600 data-[state=on]:text-white inline-flex size-9 items-center justify-center transition-all active:scale-[0.98]"
+    >
+      <ArrowsOut class="size-6" />
+    </Toolbar.GroupItem>
+    <Toolbar.GroupItem
       aria-label="toggle shovel"
       value="shovel"
       class="rounded-2xl hover:bg-violet-400 active:bg-violet-300 data-[state=on]:bg-violet-600 data-[state=on]:text-white inline-flex size-9 items-center justify-center transition-all active:scale-[0.98]"
@@ -26,13 +33,6 @@
       class="rounded-2xl hover:bg-violet-400 active:bg-violet-300 data-[state=on]:bg-violet-600 data-[state=on]:text-white inline-flex size-9 items-center justify-center transition-all active:scale-[0.98]"
     >
       <Plant class="size-6" />
-    </Toolbar.GroupItem>
-    <Toolbar.GroupItem
-      aria-label="toggle viewer"
-      value="view"
-      class="rounded-2xl hover:bg-violet-400 active:bg-violet-300 data-[state=on]:bg-violet-600 data-[state=on]:text-white inline-flex size-9 items-center justify-center transition-all active:scale-[0.98]"
-    >
-      <ArrowsOut class="size-6" />
     </Toolbar.GroupItem>
   </Toolbar.Group>
 </Toolbar.Root>
